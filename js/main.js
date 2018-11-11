472e4b2d1398176a7576dffa1f29f5f891b77bd3
nicolaslopez82 <nicolaslopez82@gmail.com>
2018-11-11 00:51:16 -0800

    indexAUsar -> index

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-console.log('Happy Halloween');
-console.log('I want to eat more');
-console.log('Candy.');
 
+$(document).ready(function () {
+
+    $('#sections').change(function () {        
+        $(".grid-container").show(800);
+        $(".grid-container").css({ "display": "grid" });
+    });
+
+    $("#sections").change(function () {
+        var valueSelected = $('#sections').find(":selected").text();
+        alert(valueSelected);
+    });
+});
