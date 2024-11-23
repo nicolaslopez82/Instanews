c7f1bf20c0a9b38e693073fb7d7baf0d32f56171
nicolaslopez <nicolaslopez82@gmail.com>
2024-11-22 21:15:45 -0300

    multimedia index 2

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ $(document).ready(() => {
                     let caption = null;
 
                     //Validation for caption in null.
-                    if(value.multimedia[3].caption == ""){
+                    if(value.multimedia[2].caption == ""){
                         caption = value.title;
                     }else{
                         caption = value.multimedia[4].caption;
